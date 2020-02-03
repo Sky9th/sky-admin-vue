f4d6e9ef19b2a4ac787a9d7d7a5d8b06980906e1
Sky9th <weitianxu@qq.com>
2020-02-03 20:41:27 +0800

    [+] 用户管理功能完善

--- src/api/sys/user.js
+++ src/api/sys/user.js
@@ -7,6 +7,19 @@ const api = {
     indexByRoleId (roleId, query) {
         return request.get('user/indexByRoleId/' + roleId, { params: query })
     },
+    read (id) {
+        return request.get('users/' + id)
+    },
+    save (query) {
+        if (query.id) {
+            return request.put('users/' + query.id, query)
+        } else {
+            return request.post('users', query)
+        }
+    },
+    del (ids) {
+        return request.delete('users', { data: { ids } })
+    },
     getPermissionInfo () {
         return request.get('user/getPermissionInfo')
     }
--- src/views/system/user/editForm.vue
+++ src/views/system/user/editForm.vue
@@ -1,119 +1,96 @@
 <template>
-  <el-dialog
-    title="用户信息"
-    :visible.sync="dialogVisible"
-    @opened="dialogOpen"
-  >
-    <el-form
-      ref="form"
-      :model="form"
-      label-width="80px"
-      size="small"
-    >
-      <el-form-item
-        prop="name"
-        label="账号"
-        :rules="[{ required: true, message: '不能为空'}]"
-      >
-        <el-input v-model="form.name"></el-input>
-      </el-form-item>
-      <el-form-item
-        prop="trueName"
-        label="用户名称"
-        :rules="[{ required: true, message: '不能为空'}]"
-      >
-        <el-input v-model="form.trueName"></el-input>
-      </el-form-item>
-      <el-form-item
-        prop="email"
-        label="邮箱"
-      >
-        <el-input v-model="form.email"></el-input>
-      </el-form-item>
-      <el-form-item
-        prop="phone"
-        label="phone"
-      >
-        <el-input v-model="form.phone"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button
-          type="primary"
-          :loading="loading"
-          @click="saveUser"
-        >保存</el-button>
-        <el-button @click="close">取消</el-button>
-      </el-form-item>
-    </el-form>
-
-  </el-dialog>
-
+    <el-dialog title="用户信息" :visible.sync="dialogVisible" @opened="dialogOpen" @close="close" :destroy-on-close="true">
+        <el-form ref="form" :model="form" label-width="80px" size="small">
+            <el-form-item prop="username" label="账号" :rules="[{ required: true, message: '不能为空'}]">
+                <el-input v-model="form.username" />
+            </el-form-item>
+            <el-form-item prop="password" label="密码">
+                <el-input v-model="form.password" type="password" />
+            </el-form-item>
+            <el-form-item prop="repassword" label="确认密码">
+                <el-input v-model="form.repassword" type="password" />
+            </el-form-item>
+            <el-form-item prop="realname" label="姓名">
+                <el-input v-model="form.realname" />
+            </el-form-item>
+            <el-form-item prop="nickname" label="昵称">
+                <el-input v-model="form.nickname" />
+            </el-form-item>
+            <el-form-item prop="email" label="邮箱">
+                <el-input v-model="form.email" />
+            </el-form-item>
+            <el-form-item prop="phone" label="手机">
+                <el-input v-model="form.phone" />
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" :loading="loading" @click="saveUser">保存</el-button>
+                <el-button @click="close">取消</el-button>
+            </el-form-item>
+        </el-form>
+    </el-dialog>
 </template>
 <script>
-import * as userService from "@/api/sys/user";
-export default {
-  name: "userEditForm",
-  props: {
-    user: Object,
-    value: Boolean
-  },
-  data() {
-    return {
-      loading: false,
-      dialogVisible: false,
-      form: {
-        name: "",
-        trueName: "",
-        phone: "",
-        email: ""
-      }
-    };
-  },
-  watch: {
-    value(val) {
-      this.dialogVisible = val;
-    },
-    dialogVisible(val) {
-      this.$emit("input", val);
-    }
-  },
-  methods: {
-    dialogOpen() {
-      this.$refs.form.resetFields();
-      if (this.user.id) {
-        userService.getUser(this.user.id).then(data => {
-          let form = {};
-          form.name = data.name;
-          form.trueName = data.trueName;
-          form.phone = data.phone;
-          form.email = data.email;
-          this.form = form;
-        });
-      } else {
-        this.form = {};
-      }
-    },
-    saveUser() {
-      this.$refs["form"].validate(valid => {
-        if (valid) {
-          this.loading = true;
-          userService
-            .saveUser({ ...this.form, id: this.user.id })
-            .then(data => {
-              this.loading = false;
-              this.dialogVisible = false;
-              this.$emit("submit");
-            });
-        } else {
-          return false;
-        }
-      });
-    },
-    close() {
-      this.$refs["form"].resetFields();
-      this.dialogVisible = false;
-    }
-  }
-};
-</script>
+import userService from '@/api/sys/user'
 
+export default {
+    name: 'userEditForm',
+    props: {
+        user: Object,
+        value: Boolean
+    },
+    data () {
+        return {
+            loading: false,
+            dialogVisible: false,
+            form: {
+                name: '',
+                trueName: '',
+                phone: '',
+                email: ''
+            }
+        }
+    },
+    watch: {
+        value (val) {
+            this.dialogVisible = val
+        },
+        dialogVisible (val) {
+            this.$emit('input', val)
+        }
+    },
+    methods: {
+        dialogOpen () {
+            this.$refs.form.resetFields()
+            if (this.user.id) {
+                userService.read(this.user.id).then(data => {
+                    this.form = data
+                })
+            } else {
+                this.form = {}
+            }
+        },
+        saveUser () {
+            this.$refs['form'].validate(valid => {
+                if (valid) {
+                    this.loading = true
+                    userService
+                        .save({ ...this.form, id: this.user.id })
+                        .then(data => {
+                            this.loading = false
+                            this.dialogVisible = false
+                            this.$emit('submit')
+                        }).catch(err => {
+                            this.loading = false
+                        })
+                } else {
+                    return false
+                }
+            })
+        },
+        close () {
+            this.$refs['form'].resetFields()
+            this.dialogVisible = false
+        }
+    }
+}
+</script>
