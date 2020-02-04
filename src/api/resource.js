783ce45c51f9c78b4f5345bc565f4f0740a05384
Sky9th <weitianxu@qq.com>
2020-02-04 11:11:43 +0800

    [+] 固定接口生成优化

---
+++ src/api/resource.js
@@ -0,0 +1,22 @@
+import request from '@/plugin/axios'
+
+export function resource (router) {
+    return {
+        index (query) {
+            return request.get(router, { params: query })
+        },
+        read (id) {
+            return request.get(router + '/' + id)
+        },
+        save (query) {
+            if (query.id) {
+                return request.put(router + '/' + query.id, query)
+            } else {
+                return request.post(router, query)
+            }
+        },
+        del (ids) {
+            return request.delete(router, { data: { ids } })
+        }
+    }
+}
