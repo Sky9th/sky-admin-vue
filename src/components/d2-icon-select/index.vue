2d0a0ec87e3be7e94ac4a3848c42b584e2774ff7
Sky9th <weitianxu@qq.com>
2020-02-13 11:19:41 +0800

    [!] 修复图标选择器的不可用状态异常

--- src/components/d2-icon-select/index.vue
+++ src/components/d2-icon-select/index.vue
@@ -1,76 +1,53 @@
 <template>
   <span>
-    <el-popover
-      ref="pop"
-      v-model="pop"
-      :placement="placement"
-      width="300"
-      trigger="click">
-      <el-row type="flex" justify="end" class="d2-mb-10" v-if="clearable">
-        <el-button
-          type="danger"
-          icon="el-icon-delete"
-          size="mini"
-          class="d2-fr"
-          @click="selectIcon()">
-          清空
-        </el-button>
-      </el-row>
-      <el-input
-        v-model="searchText"
-        :clearable="true"
-        placeholder="搜索 比如 'plus'"
-        prefix-icon="el-icon-search">
+      <el-popover ref="pop" v-model="pop" :placement="placement" width="300" trigger="click">
+          <el-row type="flex" justify="end" class="d2-mb-10" v-if="clearable">
+              <el-button type="danger" icon="el-icon-delete" size="mini" class="d2-fr"
+                         @click="selectIcon()">清空</el-button>
+          </el-row>
+          <el-input v-model="searchText" :clearable="true" placeholder="搜索 比如 'plus'"
+                    prefix-icon="el-icon-search"></el-input>
+          <el-collapse v-if="!searchMode" class="d2-icon-select--group" v-model="collapseActive">
+              <el-collapse-item v-for="(item, index) in icon" :key="index" :title="item.title" :name="index" class="d2-icon-select--class">
+                  <el-row class="d2-icon-select--class-row"><el-col
+                          class="d2-icon-select--class-col" v-for="(iconName, iconIndex) in item.icon" :key="iconIndex" :span="4"
+                          @click.native="selectIcon(iconName)">
+                      <i :class="'fa fa-' + iconName"></i>
+                  </el-col>
+                  </el-row>
+              </el-collapse-item>
+          </el-collapse>
+          <div v-if="searchMode" class="d2-icon-select--group">
+              <div class="d2-icon-select--class" v-for="(item, index) in iconFilted" :key="index">
+                  <div class="d2-icon-select--class-title">{{item.title}}</div>
+                  <el-row class="d2-icon-select--class-row">
+                      <el-col class="d2-icon-select--class-col" v-for="(iconName, iconIndex) in item.icon" :key="iconIndex" :span="4" @click.native="selectIcon(iconName)"><i
+              :class="'fa fa-' + iconName"></i>
+                      </el-col>
+                  </el-row>
+              </div>
+          </div>
+      </el-popover>
+      <!-- 允许用户输入 -->
+      <el-input v-if="userInput" v-model="currentValue" v-bind="bind" style="max-width: 240px;" :disabled="disabled">
+          <template v-if="value" slot="prepend"><i :class="'fa fa-' + value"></i></template>
+          <el-button v-popover:pop slot="append"><i class="fa fa-list"></i></el-button>
       </el-input>
-      <el-collapse v-if="!searchMode" class="d2-icon-select--group" v-model="collapseActive">
-        <el-collapse-item v-for="(item, index) in icon" :key="index" :title="item.title" :name="index" class="d2-icon-select--class">
-          <el-row class="d2-icon-select--class-row">
-            <el-col class="d2-icon-select--class-col" v-for="(iconName, iconIndex) in item.icon" :key="iconIndex" :span="4" @click.native="selectIcon(iconName)">
-              <i :class="'fa fa-' + iconName"></i>
-            </el-col>
-          </el-row>
-        </el-collapse-item>
-      </el-collapse>
-      <div v-if="searchMode" class="d2-icon-select--group">
-        <div class="d2-icon-select--class" v-for="(item, index) in iconFilted" :key="index">
-          <div class="d2-icon-select--class-title">{{item.title}}</div>
-          <el-row class="d2-icon-select--class-row">
-            <el-col class="d2-icon-select--class-col" v-for="(iconName, iconIndex) in item.icon" :key="iconIndex" :span="4" @click.native="selectIcon(iconName)">
-              <i :class="'fa fa-' + iconName"></i>
-            </el-col>
-          </el-row>
-        </div>
-      </div>
-    </el-popover>
-    <!-- 允许用户输入 -->
-    <el-input
-      v-if="userInput"
-      v-model="currentValue"
-      v-bind="bind"
-      style="max-width: 240px;">
-      <template v-if="value" slot="prepend">
-        <i :class="'fa fa-' + value"></i>
-      </template>
-      <el-button v-popover:pop slot="append">
-        <i class="fa fa-list"></i>
+      <!-- 不允许用户输入 -->
+      <el-button v-popover:pop v-if="!userInput" :disabled="disabled">
+          <template v-if="value"><i :class="'fa fa-' + value"></i></template>
+          {{value ? value : placeholder}}
       </el-button>
-    </el-input>
-    <!-- 不允许用户输入 -->
-    <el-button v-popover:pop v-if="!userInput">
-      <template v-if="value">
-        <i :class="'fa fa-' + value"></i>
-      </template>
-      {{value ? value : placeholder}}
-    </el-button>
   </span>
 </template>
 
 <script>
 import icon from './data/index'
+
 export default {
     name: 'd2-icon-select',
     props: {
-    // 值
+        // 值
         value: {
             type: String,
             required: false,
@@ -105,6 +82,10 @@ export default {
             type: Boolean,
             required: false,
             default: true
+        },
+        disabled: {
+            type: Boolean,
+            default: false
         }
     },
     data () {
@@ -123,7 +104,7 @@ export default {
         }
     },
     computed: {
-    // 输入框上绑定的设置
+        // 输入框上绑定的设置
         bind () {
             return {
                 placeholder: this.placeholder,
@@ -163,34 +144,37 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.d2-icon-select--group {
-  max-height: 400px;
-  overflow-x: hidden;
-  overflow-y: scroll;
-  border-top: none;
-  border-bottom: none;
-  .d2-icon-select--class {
-    .d2-icon-select--class-title {
-      line-height: 30px;
-      text-align: center;
-      background-color: $color-bg;
-      border-radius: 4px;
-      margin: 10px 0px;
-    }
-    .d2-icon-select--class-row {
-      .d2-icon-select--class-col {
-        line-height: 40px;
-        text-align: center;
-        color: $color-text-sub;
-        &:hover {
-          color: $color-text-main;
-          background-color: $color-bg;
-          border-radius: 4px;
-          font-size: 26px;
-          box-shadow: inset 0px 0px 0px 1px $color-border-1;
+    .d2-icon-select--group {
+        max-height: 400px;
+        overflow-x: hidden;
+        overflow-y: scroll;
+        border-top: none;
+        border-bottom: none;
+
+        .d2-icon-select--class {
+            .d2-icon-select--class-title {
+                line-height: 30px;
+                text-align: center;
+                background-color: $color-bg;
+                border-radius: 4px;
+                margin: 10px 0px;
+            }
+
+            .d2-icon-select--class-row {
+                .d2-icon-select--class-col {
+                    line-height: 40px;
+                    text-align: center;
+                    color: $color-text-sub;
+
+                    &:hover {
+                        color: $color-text-main;
+                        background-color: $color-bg;
+                        border-radius: 4px;
+                        font-size: 26px;
+                        box-shadow: inset 0px 0px 0px 1px $color-border-1;
+                    }
+                }
+            }
         }
-      }
     }
-  }
-}
 </style>
