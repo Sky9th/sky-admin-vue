60c18cdf0a8233a8aecce41022c2d86a95ddb4c1
Sky9th <weitianxu@qq.com>
2020-02-19 11:25:41 +0800

    [+] 登录验证码

--- src/api/sys/login.js
+++ src/api/sys/login.js
@@ -3,3 +3,7 @@ import request from '@/plugin/axios'
 export function AccountLogin (data) {
     return request.post('/login', data)
 }
+
+export function LoginVerify (name) {
+    return request.get('/verifyImg/' + name)
+}
